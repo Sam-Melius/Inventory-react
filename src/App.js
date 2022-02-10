33a76d2e384b7efaef495e4e125d16988bb6a586
Sam-Melius <91688565+Sam-Melius@users.noreply.github.com>
2022-02-10 10:58:41 -0700

    wrote useEffect and handleLogout in app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import { getUser, logout } from './services/fetch-utils';
 
-function App() {
+export default function App() {
+  const [user, setUser] = useState(localStorage.getItem('supabase.auth.token'));
+
+  useEffect(() => {
+    async function fetch() {
+      const fetchUser = await getUser();
+      if (fetchUser) setUser(fetchUser);
+    }
+    fetch();
+  }, []);
+
+  async function handleLogout() {
+    await logout();
+    setUser();
+  }
+
   return (
+    
+
     <div className="App">
       
     </div>
   );
 }
 
-export default App;
