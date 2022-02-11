5300bb7d9bef8797959ee30a3c3c7d9b90068416
Sam-Melius <91688565+Sam-Melius@users.noreply.github.com>
2022-02-11 10:11:09 -0700

    wrote app.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 import { useEffect, useState } from 'react';
 import { getUser, logout } from './services/fetch-utils';
+import { BrowserRouter as Router,
+  NavLink,
+  Switch,
+  Redirect,
+  Route
+} from 'react-router-dom';
+import AuthPage from './AuthPage';
+import ListPage from './ListPage';
+import DetailPage from './DetailPage';
+import CreatePage from './CreatePage';
 
 export default function App() {
   const [user, setUser] = useState(localStorage.getItem('supabase.auth.token'));
@@ -21,10 +31,59 @@ export default function App() {
 
   return (
     
-
-    <div className="App">
-      
-    </div>
+    <Router>
+      <div className="App">
+        <header>
+          {
+            user &&
+            <ul>
+              <li>
+                <NavLink to='/bands'>List Page</NavLink>
+              </li>
+              <li>
+                <NavLink to='/create'>Create Page</NavLink>
+              </li>
+              <li>
+                <a onClick={handleLogout}>Logout</a>
+              </li>
+            </ul>
+          }
+        </header>
+        <main>
+          <Switch>
+            <Route exact path='/'>
+              {
+                user
+                  ? <Redirect to='list'/>
+                  : <AuthPage setUser={setUser} />
+              }
+            </Route>
+            <Route exact path='/bands'>
+              {
+                !user
+                  ? <Redirect to='/'/>
+                  : <ListPage />
+              }
+            </Route>
+            <Route exact path='/bands/:id'>
+              {
+                !user
+                  ? <Redirect to='/'/>
+                  : <DetailPage />
+              }
+            </Route>
+            <Route exact path='/create'>
+              {
+                !user
+                  ? <Redirect to='/'/>
+                  : <CreatePage />
+              }
+            </Route>
+          </Switch>
+        </main>
+      </div>
+    </Router>
+    
   );
 }
 
