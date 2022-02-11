b660e0bf0b387278eb7d4a66a738ba21949b91e4
Sam-Melius <91688565+Sam-Melius@users.noreply.github.com>
2022-02-11 10:27:45 -0700

    wrote authPage

--- src/AuthPage.js
+++ src/AuthPage.js
@@ -1,7 +1,36 @@
-import React from 'react';
+import { useState } from 'react';
+import { signIn, signUp } from './services/fetch-utils';
 
-export default function AuthPage() {
+export default function AuthPage({ setUser }) {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  async function handleSignIn(e) {
+    e.preventDefault();
+    const newUser = await signIn(email, password);
+    setUser(newUser);
+  }
+
+  async function handleSignUp() {
+    const newUser = await signUp(email, password);
+    setUser(newUser);
+  }
+
   return (
-    <div>AuthPage</div>
+    <div>
+      <h1>Bands</h1>
+      <form onSubmit={handleSignIn}>
+        <label>
+          Email
+          <input required type='email' onChange={e => setEmail(e.target.value)} name='email' />
+        </label>
+        <label>
+          Password
+          <input required type='password' onChange={e => setPassword(e.target.value)} name='password' />
+        </label>
+        <button>Sign In</button>
+        <button type='button' onClick={handleSignUp}>Sign Up</button>
+      </form>
+    </div>
   );
 }
