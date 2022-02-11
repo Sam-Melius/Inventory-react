13804005c9670b5d6b5e8c29b977093ff4124c83
Sam-Melius <91688565+Sam-Melius@users.noreply.github.com>
2022-02-11 10:43:53 -0700

    wrote band page

--- src/ListPage.js
+++ src/ListPage.js
@@ -1,7 +1,23 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
+import { getBands } from './services/fetch-utils';
+import Band from './Band';
 
 export default function ListPage() {
+  const [bands, setBands] = useState([]);
+
+  useEffect(() => {
+    async function fetch() {
+      const fecthBands = await getBands();
+      setBands(fecthBands);
+    }
+    fetch();
+  }, []);
+
   return (
-    <div>ListPage</div>
+    <div>List Of Bands:
+      {
+        bands.map(band => <Band key={band.id} band={band} />)
+      }
+    </div>
   );
 }
